b8cb3f5098bde5ef7884628dfe6b8aa479f00ddd
Zachary J. Fields <zachary_fields@yahoo.com>
2017-05-08 00:10:18 -0700

    Correct copy/paste error in StateChangeDetection

--- samples/digital/StateChangeDetection.cpp
+++ samples/digital/StateChangeDetection.cpp
@@ -29,35 +29,6 @@ This example code is in the public domain.
 
  */
 
-/* Blink without Delay
-
- Turns on and off a light emitting diode (LED) connected to a digital
- pin, without using the delay() function.  This means that other code
- can run at the same time without being interrupted by the LED code.
-
- The circuit:
- * Use the onboard LED.
- * Note: Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino model, check
-  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
-
- created 2005
- by David A. Mellis
- modified 8 Feb 2010
- by Paul Stoffregen
- modified 11 Nov 2013
- by Scott Fitzgerald
- modified 9 Jan 2017
- by Arturo Guadalupi
-
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
- */
-
 using namespace remote_wiring::boards::arduino::uno;  // change to your board
 
 // this constant won't change:
@@ -68,16 +39,6 @@ const int ledPin = 13;       // the pin that the LED is attached to
 int buttonPushCounter = 0;   // counter for the number of button presses
 int buttonState = 0;         // current state of the button
 int lastButtonState = 0;     // previous state of the button
-
-// Variables will change :
-int ledState = LOW;             // ledState used to set the LED
-
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
-
-// constants won't change :
-const long interval = 1000;           // interval at which to blink (milliseconds)
 
 void setup(remote_wiring::RemoteDevice & board) {
   // initialize the button pin as a input:
